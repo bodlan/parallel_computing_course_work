70c807d2f5ebb6ae6fef3f6cab36f7e7628614fb
bodlan <bodlankokidko@gmail.com>
2020-05-02 09:05:08 +0300

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc,char *argv[]){
+
+    std::cout<<"Count of args: "<<argc<<std::endl;
+    for(int i=0;i<argc; i++){
+        std::cout<<argv[i]<<std::endl;
+    }
+    return 0;
+
+}
