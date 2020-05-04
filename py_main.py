483d4b62e5605ffaa2c229170d8cf4e26965b993
bodlan <bodlankokidko@gmail.com>
2020-05-04 12:09:17 +0300

    feat: file reading and spliting into list

--- py_main.py
+++ py_main.py
@@ -2,16 +2,23 @@ import sys
 import glob
 import errno
 
-path = 'C:\\Users\\bodlan\\Desktop\\aclImdb\\test\\pos\\*.txt'
-files = glob.glob(path)
-count=0
-for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
-    try:
-        with open(name) as f: # No need to specify 'r': this is the default.
-            sys.stdout.write(f.read())
-            print("\n")
-            count+=1
-    except IOError as exc:
-        if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
-            raise # Propagate other kinds of IOError.
-print("count:",count)
+globalList=[]
+def main():
+    path = 'C:\\Users\\bodlan\\Desktop\\aclImdb\\test\\pos\\*.txt'
+    files = glob.glob(path) # list of path names that matches pathname
+    count=0
+    for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
+        try:
+            with open(name) as f: # No need to specify 'r': this is the default.
+                # sys.stdout.write(f.read())
+                string =f.read().split(" ")
+                print(string)
+                count+=1
+        except IOError as exc:
+            if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
+                raise # Propagate other kinds of IOError.
+    print("count:",count)
+
+
+if __name__ == '__main__':
+    main()
