d607563f9f47d2422fd2a62e76a008673342bb13
bodlan <bodlankokidko@gmail.com>
2020-05-04 13:51:43 +0300

    feat: words saving in dict

--- py_main.py
+++ py_main.py
@@ -2,7 +2,7 @@ import sys
 import glob
 import errno
 
-globalList=[]
+globalList={}
 def main():
     path = 'C:\\Users\\bodlan\\Desktop\\aclImdb\\test\\pos\\*.txt'
     files = glob.glob(path) # list of path names that matches pathname
@@ -11,14 +11,22 @@ def main():
         try:
             with open(name) as f: # No need to specify 'r': this is the default.
                 # sys.stdout.write(f.read())
-                string =f.read().split(" ")
-                print(string)
+                words =f.read().split(" ")
+                for word in words:
+                    if word in globalList:
+                        globalList['{}'.format(word)].append(str(count))
+                    else:
+                        globalList['{}'.format(word)]=list(str(count))
                 count+=1
         except IOError as exc:
             if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
                 raise # Propagate other kinds of IOError.
-    print("count:",count)
-
+    print("count:",count,"\n globalList starts:\n")
+    w=0
+    for i in globalList:
+        print(i," : ",globalList[i],"\n")
+        w+=1
+    print('w:',w)
 
 if __name__ == '__main__':
     main()
