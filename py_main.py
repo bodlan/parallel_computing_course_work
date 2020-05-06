a4c60e58577ad855bfc7f43e1dba672e7b8589e3
bodlan <bodlankokidko@gmail.com>
2020-05-06 13:33:33 +0300

    feat: frequency count of word in dict

--- py_main.py
+++ py_main.py
@@ -1,19 +1,26 @@
 import sys
 import glob
+import re
 import errno
-from  collections import defaultdict
+from collections import defaultdict
 
-globalList=defaultdict(list)
+
 def main():
+    globalList = defaultdict(list)
     path = 'C:\\Users\\bodlan\\Desktop\\aclImdb\\test\\pos\\*.txt'
     files = glob.glob(path) # list of path names that matches pathname
-    count=0
+
     for name in files: # 'file' is a builtin type, 'name' is a less-ambiguous variable name.
         try:
-            with open(name) as f: # No need to specify 'r': this is the default.
+            with open(name) as f:
+                #print("name: ",name)
+                #setting variable count to txt number.
+                count=int(re.findall(r'\d+|$',name)[0])
                 # sys.stdout.write(f.read())
                 words =f.read().split(" ")
                 for word in words:
+                    #clear words in text
+                    word=re.findall(r'\w+|$',word)[0]
                     if word in globalList:
                         globalList['{}'.format(word)].append(count)
                     else:
@@ -22,13 +29,17 @@ def main():
         except IOError as exc:
             if exc.errno != errno.EISDIR: # Do not fail if a directory is found, just ignore it.
                 raise # Propagate other kinds of IOError.
-    print("count:",count,"\n globalList starts:\n")
+    print("globalList starts:\n")
     w=0
     for i in globalList:
         print(i," : ",globalList[i],"\n")
         w+=1
-    print('w:',w)
-    print()
+    print("w: ",w)
+    #count frequency of word
+    for key,value in globalList.items():
+        print(key,len([item for item in value if item]))
+
+
 
 if __name__ == '__main__':
     main()
